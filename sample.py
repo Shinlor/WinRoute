e4e909366fa478743faf11033646b1602b2a8d13
Shinlor <shinlor@outlook.com>
2016-10-17 21:13:40 +0800

    class winroute 便于初始化得到默认路由

--- sample.py
+++ sample.py
@@ -3,10 +3,11 @@
 
 import winroute.winroute as winroute
 
-
+#实例
+route=winroute.WinRoute()
 
 #输出当前路由表（IPv4）
-winroute.printroute()
+route.printroute()
 
 #添加一个路由，给出：目标IP，子网掩码，网关(即下一跳IP，不给出与默认路由的相同)，Metric值（不给出则与默认路由的相同），接口（不给出则与默认路由的相同）
-winroute.CreateIpForwardEntry("1.0.7.0","255.255.255.0","192.168.1.1")
+route.CreateIpForwardEntry("1.0.7.0","255.255.255.0","192.168.1.1")
